39c8dc321a37229e8b1e5fdad47313fbc29ffa18
BisCodess <pereiracoelhobeatriz@gmail.com>
2024-10-23 21:11:48 -0300

    Second commit

--- script.js
+++ script.js
@@ -3,19 +3,13 @@
       try {
         const response = await fetch('https://jsonplaceholder.typicode.com/users');//  requisição HTTP para a API e (await)aguarda a resposta
         const users = await response.json();// Converte a resposta em JSON e aguarda o resultado
-        const tableBody = document.querySelector('#userTable tbody');// seleciona o tbody da table
+        const tableBody = document.querySelector('#userTable tbody');// obtem o tbody da table
         //console.log(tableBody);
         
     
-        // Itera sobre o array de usuários retornado pela API
+        // Itera sobre o array de usuários
         users.forEach(user => {
           const row = document.createElement('tr');// Cria uma nova linha no tbody
-
-          // Formata o endereço completo,coordenadas,company do usuário como uma string 
-    
-          const addresss = user.address.street + ", " + user.address.suite + ", "+ user.address.city +  ", "+user.address.zipcode ;
-          const coodenadas = "Lat:" + user.address.geo.lat + ", Lng:"+ user.address.geo.lng;
-          const companyy = user.company.name + ", " + user.company.catchPhrase;
 
           // Preenche a linha da tabela com os dados do usuário
           row.innerHTML = ` 
@@ -30,7 +24,7 @@
             <td>` + user.company.name + " - " + user.company.catchPhrase + `</td>         <!-- Nome e slogan da empresa -->
           `;
     
-          // Adiciona a nova linha ao <tbody> da tabela
+          // Adiciona a nova linha ao tbody da tabela
           tableBody.appendChild(row);
         });
     
@@ -39,13 +33,11 @@
         console.error('Erro ao buscar usuários:', error);
     
         // Exibe uma mensagem de erro na tabela se a API falhar
-        const tableBody = document.querySelector('#userTable tbody');// optem o body da table
+        const tableBody = document.querySelector('#userTable tbody');// obtem o body da table
         tableBody.innerHTML = '<tr><td colspan="9">Erro ao carregar dados.</td></tr>';//dentro do body table /cria linha e msg
       }
     }
     
-    // Adiciona um "listener" para executar a função fetchUsers assim que a página carregar
-    //window.addEventListener('DOMContentLoaded', fetchUsers); entender
-    fetchUsers()
+    fetchUsers();
    
     
